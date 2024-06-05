5a0590ef2e56cd68dfdc521813593ac62a7691df
Alexander Giannini <alexgiannini@Alexanders-MBP.lan>
2024-06-05 11:48:40 -0700

    added more tests

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,12 +1,14 @@
 import { test, expect, defineConfig } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('/');
+})
+
+test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/ESPN - Serving Sports Fans. Anytime. Anywhere./);
 });
 
 test('conduct a search on the homepage', async ({ page }) => {
-  await page.goto('/');
   await page.locator("//a[@id='global-search-trigger']").click();
   await page.locator("//input[@id='global-search-input']").fill('Lakers');
   await page.locator("//input[@id='global-search-input']").press('Enter');
@@ -14,8 +16,18 @@ test('conduct a search on the homepage', async ({ page }) => {
 });
 
 test('navigate to Lakers page via NBA dropdown', async ({ page }) => {
-  await page.goto('/');
   await page.locator("//header/nav[@id='global-nav']/ul[1]/li[2]").hover();
   await page.locator("//span[contains(text(),'Los Angeles Lakers')]").click();
   await expect(page).toHaveURL(/los-angeles-lakers/);
-})
+});
+
+test('should select Los Angeles when hovering over More option', async ({ page }) => {
+  await page.locator('.more-espn').hover();
+  await page.locator("//a[@name='&lpos=sitenavdefault+more_los_angeles']").click();
+  await expect(page.locator("//h2[contains(text(),'Los Angeles teams')]")).toBeVisible();
+});
+
+test('should click first article within Top Headlines section', async ({ page }) => {
+  await page.locator("//a[@name='&lpos=fp:feed:1:coll:headlines:1']").click();
+  await expect(page.locator('.article-header')).toBeVisible();
+});
